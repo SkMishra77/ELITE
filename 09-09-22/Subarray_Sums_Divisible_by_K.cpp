1a1228e4ed62234742c6655cc77034cd4ead5bd8
SkMishra77 <sanatkumarmishra02@gmail.com>
2022-09-11 01:36:18 +0530

    cpp

---
+++ 09-09-22/Subarray_Sums_Divisible_by_K.cpp
@@ -0,0 +1,52 @@
+
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& arr, int k) {
+        int n;
+        n=arr.size();
+        vector<int>ps;
+        int sum=0;
+        map<int,int> d;
+        map<int,int> ::iterator j;
+        
+        for (int  i = 0; i < n; i++)
+        {
+            sum+=arr[i];
+            ps.push_back(sum);
+        }
+        for (int i = 0; i < n; i++)
+        {
+            ps[i]=ps[i]%k;
+            if (ps[i]<0)
+            {
+                ps[i]=k+ps[i];
+            }
+        }
+    
+        for (int  i = 0; i < n; i++)
+        {
+            if(d.find(ps[i])== d.end())
+            {
+                d[ps[i]]=1;
+            }
+            else{
+                d[ps[i]]++;
+            }
+        }
+        int ans=0;
+        for(j=d.begin();j!=d.end();++j)
+        {
+            if(j->first !=0)
+            {
+                ans+=((j->second)*(j->second -1))/2;
+            }
+            else if (j->first ==0 || (k%2==0 && j->first == k/2))
+            {
+                ans+=((j->second)*(j->second +1))/2;
+            }
+        }
+        return ans;
+        
+            
+    }
+};
