cd07243a23786864acfe0b34981dc86a9c271a7c
SkMishra77 <sanatkumarmishra02@gmail.com>
2022-09-13 17:53:52 +0530

    13

--- 09-09-22/Subarray_Sums_Divisible_by_K.cpp
+++ 09-09-22/Subarray_Sums_Divisible_by_K.cpp
@@ -50,3 +50,47 @@ public:
             
     }
 };
+
+// satvik soln
+
+
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& arr, int k) {
+        int n;
+        n=arr.size();
+        vector<int>ps;
+        int sum=0;
+        map<int,int> d;
+        // map<int,int> ::iterator j;
+        
+        for (int  i = 0; i < n; i++)
+        {
+            sum+=arr[i];
+            ps.push_back(sum);
+            ps[i]=ps[i]%k;
+            if (ps[i]<0)
+            {
+                ps[i]=k+ps[i];
+            }
+            d[ps[i]]++;
+        }
+
+        int ans=0;        
+        for(auto j:d)
+        {
+            if(j.first !=0)
+            {
+                ans+=((j.second)*(j.second -1))/2;
+            }
+            else if (j.first ==0 || (k%2==0 && j.first == k/2))
+            {
+                ans+=((j.second)*(j.second +1))/2;
+            }
+            
+        }
+        return ans;
+        
+            
+    }
+};
